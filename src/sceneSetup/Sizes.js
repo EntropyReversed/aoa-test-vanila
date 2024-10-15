783a8ba202e5a673290e8d308dd3fde52fe6922f
Stefan <atila35743@gmail.com>
2024-10-15 15:30:03 +0200

    test

--- src/sceneSetup/Sizes.js
+++ src/sceneSetup/Sizes.js
@@ -8,29 +8,25 @@ export default class Sizes {
     this.widthInit = width || window.innerWidth;
     this.heightinit = height || window.innerHeight;
 
-
-    this.homeSection = document.querySelector('#home');
-    this.test = document.createElement('div');
-    this.test.style.position = 'fixed';
-    this.test.style.top = '4rem';
-    this.test.style.left = 0;
-    this.test.style.zIndex = 1000;
-    this.test.style.color = 'white';
-    this.test.style.textShadow = '0 0 2px black';
-    document.body.appendChild(this.test);
+    this.createScreenSizer()
     this.init();
+  }
+
+  createScreenSizer() {
+    this.screenSizer = document.createElement('div');
+    this.screenSizer.style.position = 'fixed';
+    this.screenSizer.style.top = '0';
+    this.screenSizer.style.left = '0';
+    this.screenSizer.style.width = '1px';
+    this.screenSizer.style.height = '100vh';
+    this.screenSizer.style.background = 'red';
+    document.body.appendChild(this.screenSizer);
   }
 
   setSizes(width, height) {
     const { innerWidth, innerHeight } = window;
     this.width = this.manualResize ? (width ?? this.widthInit) : innerWidth;
-    this.height = this.manualResize ? (height ?? this.heightinit) : this.homeSection.clientHeight;
-    this.test.innerHTML = `
-      this.height: ${this.height};
-      innerHeight: ${innerHeight};
-    `
-
-    console.dir(document.body)
+    this.height = this.manualResize ? (height ?? this.heightinit) : this.screenSizer.clientHeight;
 
     this.aspect = this.width / this.height;
     this.pixelRatio = Math.min(window.devicePixelRatio, 2);
